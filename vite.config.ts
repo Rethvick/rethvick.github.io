18e5639503efd9b7bdf5708a13f053ca09221981
rethvick <furyassassinofficial@gmail.com>
2025-11-21 18:03:11 -0500

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    // Important for correct MIME types on many hosts
-    build: {
-        target: "esnext",
-        outDir: "dist",
-        assetsDir: "assets",
-    },
-
+    base: "/",
     server: {
         host: "::",
         port: 8080,
     },
-
-    plugins: [
-        react(),
-        mode === "development" && componentTagger(),
-    ].filter(Boolean),
-
+    plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
